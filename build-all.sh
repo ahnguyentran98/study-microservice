9480c6669a18b8ff7bb9ece02cb1e056e83e0baa
ahnguyentran98 <linguini1998@gmail.com>
2025-07-15 15:26:29 +0000

    fix: config gradle

--- build-all.sh
+++ build-all.sh
@@ -2,7 +2,22 @@
 
 echo "Building all microservices..."
 
+echo "Config gradle version to 7.6.1"
+sudo apt update
+
+sudo apt install zip unzip curl
+
+curl -s "https://get.sdkman.io" | bash
+
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+sdk install gradle 7.6.1
+
+sdk use gradle 7.6.1
+
+echo "Config gradle version to 7.6.1 done"
 # Stop any existing Gradle daemons
+echo "Stop all gradle daemons"
 gradle --stop
 
 # Build Java services
