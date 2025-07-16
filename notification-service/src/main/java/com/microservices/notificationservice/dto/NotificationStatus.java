cf82c41d1a5486e9018b0c12fd4efe09a0dbb589
ahnguyentran98 <linguini1998@gmail.com>
2025-07-16 11:07:33 +0700

    restart

--- notification-service/src/main/java/com/microservices/notificationservice/dto/NotificationStatus.java
+++
@@ -1,8 +0,0 @@
-package com.microservices.notificationservice.dto;
-
-public enum NotificationStatus {
-    PENDING,
-    SENT,
-    FAILED,
-    DELIVERED
-}
--- notification-service/src/main/java/com/microservices/notificationservice/dto/NotificationType.java
+++
@@ -1,7 +0,0 @@
-package com.microservices.notificationservice.dto;
-
-public enum NotificationType {
-    EMAIL,
-    SMS,
-    PUSH_NOTIFICATION
-}
